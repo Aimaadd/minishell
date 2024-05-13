36acc77d936871a1cfd42cbae84f4dad147aa045
Aimaadd <aimadbentayeb@gmail.Com>
2024-05-13 11:39:25 +0200

    prompt function

--- code/src/main.c
+++ code/src/main.c
@@ -3,19 +3,17 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: abentaye <abentaye@student.s19.be >        +#+  +:+       +#+        */
+/*   By: abentaye <abentaye@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/10 17:41:56 by abentaye          #+#    #+#             */
-/*   Updated: 2024/05/10 18:03:39 by abentaye         ###   ########.fr       */
+/*   Updated: 2024/05/10 23:19:46 by abentaye         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "../header/minishell.h"
+
 int	main(void)
 {
-	int	i;
-
-	i = 0;
-	while (i < 10)
-		i++;
-	return (i);
+	prompt();
+	return (0);
 }
