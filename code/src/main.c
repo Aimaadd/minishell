1766538d1a5703c2ebb624427e647df4494a60c9
m3taaa <meerbergenmathieu@proton.me>
2024-05-14 10:42:28 +0200

    add parametre for main

--- code/src/main.c
+++ code/src/main.c
@@ -6,13 +6,13 @@
 /*   By: abentaye <abentaye@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/10 17:41:56 by abentaye          #+#    #+#             */
-/*   Updated: 2024/05/10 23:19:46 by abentaye         ###   ########.fr       */
+/*   Updated: 2024/05/14 10:40:23 by mmeerber         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../header/minishell.h"
 
-int	main(void)
+int	main(int ac, char **ag, char **envp)
 {
 	prompt();
 	return (0);
