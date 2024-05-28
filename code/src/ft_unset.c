e7bb7175ea9e6a70fd00b41bdfc940a262878692
m3taaa <meerbergenmathieu@proton.me>
2024-05-28 09:50:49 +0200

    ft_unset work

--- code/src/ft_unset.c
+++ code/src/ft_unset.c
@@ -2,19 +2,28 @@
 
 void	delete_variable(char *variable, t_env *copy_env)
 {
-	t_env	tmp;
-	t_env	tmp2;
+	t_env	*tmp;
+	t_env	*variable_before;
+	t_env	*variable_after;
 	int		len;
+	int		check;
 
 	tmp = copy_env;
 	while (tmp)
 	{
-		len = check_size(tmp->variable, tmp->next->variable);
-		if (ft_strncmp(tmp->variable, tmp->next->variable, len) = 0)
-			break ;
+		len = check_size(variable, tmp->variable);
+		check = ft_strncmp(variable, tmp->variable, len);
+		if (check == 0)
+		{
+			variable_after = tmp->next;
+			break;
+		}
+		variable_before = tmp;
 		tmp = tmp->next;
 	}
-	tmp2 = tmp->next;
+	variable_before->next = variable_after;
+	if (tmp != NULL)
+		free(tmp);
 }
 
 void	ft_unset(char *variable, t_env **copy_env)
@@ -22,5 +31,5 @@ void	ft_unset(char *variable, t_env **copy_env)
 	if (variable == NULL || variable[0] == 0)
 		return ;
 	if (ft_getenv(*copy_env, variable))
-		delete_variable();
+		delete_variable(variable, *copy_env);
 }
