882f240cb0841237a31e48118bec1c71e075ecfb
Aimaadd <abentaye@shi-r11-p1.s19.be>
2024-08-29 19:09:27 +0200

    command is not reachable for some reasons

--- code/src/execute.c
+++ code/src/execute.c
@@ -6,7 +6,7 @@
 /*   By: abentaye <abentaye@student.s19.be >        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/11 08:54:38 by abentaye          #+#    #+#             */
-/*   Updated: 2024/08/28 18:36:32 by abentaye         ###   ########.fr       */
+/*   Updated: 2024/08/29 18:23:44 by abentaye         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,15 +20,17 @@ void	*setup_execute(void)
 	return (NULL);
 }
 
-int	run_execute(t_cmd *command, t_list *list)
+int	run_execute(t_list *list)
 {
 	t_list	*tmp;
 
 	tmp = list;
-	if (!command->next)
-		simple_command(command); // to keep
+	if (!g_ms->cmd->next)
+		simple_command(g_ms->cmd); // to keep
 	else
-		multiple_command(command);
+	{
+		multiple_command(g_ms->cmd);
+	}
 	if (tmp->type == HEREDOC)
 		handle_heredoc(tmp->next->content);
 	return (0);
@@ -42,10 +44,9 @@ int	execute(void)
 	g_ms->cmd->args = fill_array_from_list(g_ms->list);
 	g_ms->cmd->envp = fill_array_from_env(g_ms->cmd->env_copy);
 	get_number_command(g_ms->cmd);
-	run_execute(g_ms->cmd, g_ms->list);
+	run_execute(g_ms->list);
 	// if (g_ms->cmd->next)
 	// 	free_command(g_ms->cmd);
-	// else
-	// 	free_command(g_ms->cmd);
+	// free(g_ms->cmd);
 	return (0);
 }
--- code/src/multiple_command.c
+++ code/src/multiple_command.c
@@ -6,96 +6,106 @@
 /*   By: abentaye <abentaye@student.s19.be >        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/02 15:16:59 by abentaye          #+#    #+#             */
-/*   Updated: 2024/08/28 18:35:38 by abentaye         ###   ########.fr       */
+/*   Updated: 2024/08/29 18:29:51 by abentaye         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../header/minishell.h"
 
-static t_data_multiple	*init_data_multiple(t_cmd *command)
+static t_data_multiple *init_data_multiple(t_cmd *command)
 {
-	t_data_multiple	*data;
+    t_data_multiple *data;
 
-	data = NULL;
-	if (!data)
-		return (NULL);
-	data->number_of_command = get_number_command(command);
-	data->count_command = 0;
-	data->count_pid = 0;
-	data->last_fd = -1;
-	return (data);
+    data = (t_data_multiple *)malloc(sizeof(t_data_multiple));
+    if (!data)
+        return (NULL);
+    data->number_of_command = get_number_command(command);
+    data->count_command = 0;
+    data->count_pid = 0;
+    data->last_fd = -1;
+    return (data);
 }
 
-static void	ft_waitpid(t_data_multiple *data, pid_t *pid)
+static void ft_waitpid(t_data_multiple *data, pid_t *pid)
 {
-	while (data->count_pid < data->count_command)
-	{
-		waitpid(pid[data->count_pid], 0, 0);
-		data->count_pid++;
-	}
+    while (data->count_pid < data->count_command)
+    {
+        waitpid(pid[data->count_pid], 0, 0);
+        data->count_pid++;
+    }
 }
 
-void	run_child(pid_t *pid, t_data_multiple *data, int *fd, t_cmd *tmp_commnd)
+void run_child(pid_t *pid, t_data_multiple *data, int *fd, t_cmd *tmp_command)
 {
-	pid[data->count_command] = fork();
-	if (pid[data->count_command] == -1)
-		exit (1);
-	if (pid[data->count_command] == 0)
-	{
-		if (data->last_fd != -1)
-		{
-			dup2(data->last_fd, STDIN_FILENO);
-			close(data->last_fd);
-		}
-		if (tmp_commnd->next)
-		{
-			close(fd[0]);
-			dup2(fd[1], STDOUT_FILENO);
-			close(fd[1]);
-		}
-		simple_command(tmp_commnd);
-		exit (0);
-	}
+    pid[data->count_command] = fork();
+    if (pid[data->count_command] == -1)
+        exit(1);
+    if (pid[data->count_command] == 0)
+    {
+        if (data->last_fd != -1)
+        {
+            dup2(data->last_fd, STDIN_FILENO);
+            close(data->last_fd);
+        }
+        if (tmp_command->next)
+        {
+            close(fd[0]);
+            dup2(fd[1], STDOUT_FILENO);
+            close(fd[1]);
+        }
+        simple_command(tmp_command);
+        exit(0);
+    }
 }
 
-static void	check_fd(t_data_multiple *data, int *fd, t_cmd *tmp_command)
+static void check_fd(t_data_multiple *data, int *fd, t_cmd *tmp_command)
 {
-	if (data->last_fd != -1)
-	{
-		close(data->last_fd);
-	}
-	if (tmp_command->next)
-	{
-		close(fd[1]);
-		data->last_fd = fd[0];
-	}
+    if (data->last_fd != -1)
+    {
+        close(data->last_fd);
+    }
+    if (tmp_command->next)
+    {
+        close(fd[1]);
+        data->last_fd = fd[0];
+    }
 }
 
-int	multiple_command(t_cmd *command)
+int multiple_command(t_cmd *command)
 {
-	t_data_multiple	*data;
-	pid_t			*pid;
-	t_cmd			*tmp_command;
-	int				fd[2];
+    t_data_multiple *data;
+    pid_t *pid;
+    t_cmd *tmp_command;
+    int fd[2];
 
-	data = init_data_multiple(command);
-	if (!data) //STOPS HERE for what
-		return (1);
-	pid = (pid_t *)malloc(sizeof(pid_t) * data->number_of_command);
-	if (!pid)
-		return (1);	
-	tmp_command = command;
-	while (tmp_command)
-	{
-		if (tmp_command->next)
-			if (pipe(fd) == -1)
-				return (1);
-		run_child(pid, data, fd, tmp_command);
-		check_fd(data, fd, tmp_command);
-		data->count_command++;
-		tmp_command = tmp_command->next;
-	}
-	ft_waitpid(data, pid);
-	free(data);
-	return (0);
+    data = init_data_multiple(command);
+    if (!data)
+        return (1);
+    pid = (pid_t *)malloc(sizeof(pid_t) * data->number_of_command);
+    if (!pid)
+    {
+        free(data);
+        return (1);
+    }
+    tmp_command = command;
+    while (tmp_command)
+    {
+        if (tmp_command->next)
+        {
+            if (pipe(fd) == -1)
+            {
+                free(data);
+                free(pid);
+                return (1);
+            }
+        }
+        run_child(pid, data, fd, command);
+        check_fd(data, fd, tmp_command);
+        data->count_command++;
+        tmp_command = tmp_command->next;
+    }
+    ft_waitpid(data, pid);
+    free(pid);
+    free(data);
+    return (0);
 }
